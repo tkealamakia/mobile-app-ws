ab740324e56969cbbe7307978110f80c58a3c258
Tom Kealamakia <tkealamakia@overstock.com>
2022-04-10 22:52:36 -0600

    Adding post

--- src/main/java/com/tsunazumi/app/ws/ui/controller/UserController.java
+++ src/main/java/com/tsunazumi/app/ws/ui/controller/UserController.java
@@ -1,18 +1,12 @@
 package com.tsunazumi.app.ws.ui.controller;
 
 
+import com.tsunazumi.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.tsunazumi.app.ws.ui.model.response.UserRest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
@@ -35,9 +29,23 @@ public class UserController {
     return new ResponseEntity<>(returnValue, HttpStatus.OK);
   }
 
-  @PostMapping
-  public String createUser() {
-    return "create user was called";
+  @PostMapping(
+      consumes = {
+          MediaType.APPLICATION_XML_VALUE,
+          MediaType.APPLICATION_JSON_VALUE
+      },
+      produces = {
+          MediaType.APPLICATION_XML_VALUE,
+          MediaType.APPLICATION_JSON_VALUE
+      }
+  )
+  public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails) {
+    UserRest returnValue = new UserRest();
+    returnValue.setEmail(userDetails.getEmail());
+    returnValue.setFirstName(userDetails.getFirstName());
+    returnValue.setLastName(userDetails.getLastName());
+
+    return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
   }
 
   @PutMapping
